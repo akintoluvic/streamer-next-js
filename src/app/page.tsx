a9475be7f59ada613621243f4e3b96f35e166ca5
Victor Akintunde <akintoluvic@gmail.com>
2025-03-13 20:21:12 +0100

    fix render podcastlist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/landing/Header";
+import PodcastList from "@/components/landing/PodcastList";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <PodcastList />
     </>
   );
 }
